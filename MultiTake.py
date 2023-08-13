2d2c69438a6ffcb9671b5efcc014ee5d135650b5
MaciAC <macia.ac@gmail.com>
2023-08-13 20:51:54 +0200

    wip

--- MultiTake.py
+++ MultiTake.py
@@ -1,12 +1,15 @@
-from moviepy.editor import VideoFileClip, AudioFileClip
+from soundfile import read
+import cv2
+
 
 class MultiTake:
     def __init__(self, audio_path, video_paths):
         self.audio_path = audio_path
         self.video_paths = video_paths
 
-        self.audio_clip = AudioFileClip(self.audio_path)
-        self.video_clips = [VideoFileClip(video_path, audio=False) for video_path in self.video_paths]
+        self.audio_clip, self.sample_rate = read(self.audio_path, dtype='float64')
+        self.video_clips = [cv2.VideoCapture(video_path) for video_path in self.video_paths]
+
 
 
 if __name__ == "__main__":
--- VideoEditor.py
+++ VideoEditor.py
@@ -1,8 +1,10 @@
 from argparse import ArgumentParser
 from FileManager import FileManager
 from MultiTake import MultiTake
-from moviepy.editor import concatenate_videoclips, AudioClip
 from os.path import join
+
+import cv2
+import numpy as np
 
 class VideoEditor:
 
@@ -15,26 +17,55 @@ class VideoEditor:
             self.sync_video_paths,
         ) = self.file_manager.cut_videos_based_on_offsets()
         self.multitake = MultiTake(self.sync_audio_path, self.sync_video_paths)
-        self.create_new_video_with_change(join(self.base_folder, 'test.mp4'))
+        self.video_out_width = 1080
+        self.video_out_heigth = 1920
+        self.video_out_aspect_ratio = self.video_out_width / self.video_out_heigth
 
-    def create_new_video_with_change(self, output_path):
-        combined_clips = []
-        start_time = 0
-        end_time = 20
-        curr_time = start_time
-        vid_idx = 0
-        step=5
-        while curr_time < end_time:
-            video_clip = self.multitake.video_clips[vid_idx]
-            video_part = video_clip.subclip(curr_time, curr_time + step)
-            combined_clips.append(video_part.crossfadein(1))  # Add crossfade for smooth transition
-            curr_time += step
+    def create_new_video_with_scene_changes(self, output_path):
+        # Cut video clips at scene changes
+        cut_video_segments = [self.cut_video_at_scene_changes(video_clip) for video_clip in self.multitake.video_clips]
 
-        combined_video = concatenate_videoclips(combined_clips)
-        audio_clip = AudioClip(make_frame=self.multitake.audio_clip.to_soundarray, duration=self.multitake.audio_clip.duration)
-        final_video = combined_video.set_audio(audio_clip)
-        final_video.write_videofile(output_path, codec="libx264")
+        # Combine video segments while handling audio synchronization
+        combined_frames = []
+        audio_frame_idx = 0
+        for segments in zip(*cut_video_segments):
+            for frame in segments:
+                combined_frames.append(frame)
+            audio_frame_idx += len(segments)
+            #combined_frames[-1][:, :, 0] += self.multitake.audio_clip[audio_frame_idx - 1]
 
+        # Save the final video
+        self.save_final_video(output_path, combined_frames, self.multitake.sample_rate)
+
+
+    def cut_video_at_scene_changes(self, video_capture):
+        # Implement your logic here to cut the video at scene changes
+        # Return a list of video segments
+
+        # For demonstration purposes, let's split the video into 5 equal-length segments
+        duration = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        segment_duration = duration // 5
+        video_segments = []
+
+        for i in range(5):
+            start_frame = i * segment_duration
+            end_frame = (i + 1) * segment_duration
+            video_segment = self.extract_frames(video_capture, start_frame, end_frame)
+            video_segments.append(video_segment)
+
+        return video_segments
+
+    def extract_frames(self, video_capture, start_frame, end_frame):
+        # Extract frames between start_frame and end_frame
+        video_capture.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+        ret, chunk = video_capture.read(end_frame-start_frame)
+        return chunk
+
+    def save_final_video(self, output_path, frames, sample_rate):
+        out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'XVID'), sample_rate, (frames[0].shape[1], frames[0].shape[0]))
+        for frame in frames:
+            out.write(frame)
+        out.release()
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -43,4 +74,5 @@ if __name__ == "__main__":
     )
     parser.add_argument("-f", action="store_true")
     args = parser.parse_args()
-    synchronizer = VideoEditor(args.folder, args.f)
+    video_editor = VideoEditor(args.folder, args.f)
+    video_editor.create_new_video_with_scene_changes("test.mp4")
